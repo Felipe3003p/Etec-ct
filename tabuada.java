a5a78f574cb81c47e53ec1ecfc4a3e568cb71fbd
filipe Augusto <69225153+Felipe3003p@users.noreply.github.com>
2020-12-03 20:39:32 -0300

    Add files via upload

---
+++ tabuada.java
@@ -0,0 +1,18 @@
+package laçofor;
+
+public class tabuada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int cont=1, num=1;
+	        
+	        for(cont=1; cont<=10; cont++) {
+	            for(num=1; num<=10; num++) {
+	                System.out.println(cont+"x"+num+" = " +cont*num);
+	            }
+	        }
+
+
+	}
+
+}
